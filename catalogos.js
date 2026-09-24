24e9259d684f9c1d735cb48c38cf5967add16592
tlbcafe <thelittlebreakxal@gmail.com>
2026-09-24 00:16:37 -0600

    Update catalogos.js

--- catalogos.js
+++ catalogos.js
@@ -1,4 +1,21 @@
 window.catalogos={
   "promociones": [],
-  "nuevos": []
+  "nuevos": [
+    {
+      "src": "img_nuevos/prom1.jpeg",
+      "alt": "Promoción 1"
+    },
+    {
+      "src": "img_nuevos/prom2.jpeg",
+      "alt": "Promoción 2"
+    },
+    {
+      "src": "img_nuevos/prom3.jpeg",
+      "alt": "Promoción 3"
+    },
+    {
+      "src": "img_nuevos/prom4.jpeg",
+      "alt": "Promoción 4"
+    }
+  ]
 };
