325ab74baea80965b8165ba857edc389d79920c3
tlbcafe <thelittlebreakxal@gmail.com>
2026-09-24 16:25:26 -0600

    Add files via upload

--- catalogos.js
+++ catalogos.js
@@ -1,21 +1,4 @@
 window.catalogos={
   "promociones": [],
-  "nuevos": [
-    {
-      "src": "img_nuevos/prom1.jpeg",
-      "alt": "Promoción 1"
-    },
-    {
-      "src": "img_nuevos/prom2.jpeg",
-      "alt": "Promoción 2"
-    },
-    {
-      "src": "img_nuevos/prom3.jpeg",
-      "alt": "Promoción 3"
-    },
-    {
-      "src": "img_nuevos/prom4.jpeg",
-      "alt": "Promoción 4"
-    }
-  ]
+  "nuevos": []
 };
